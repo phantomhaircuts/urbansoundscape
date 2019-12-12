38c2c51b9bf7859c1f4e0b28496798474ded36ff
Erik Anderson <erikanderson@eriks-mbp-220.wireless.newschool.edu>
2019-12-12 11:55:34 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// console.log('life');
+//
+// function docReady(fn) {
+//     // see if DOM is already available
+//     if (document.readyState === "complete" || document.readyState === "interactive") {
+//         // call on next available tick
+//         setTimeout(fn, 1);
+//         // var element = document.getElementById("audioID");
+//         console.log('boop');
+//         let audio1 = document.getElementById("audioID");
+//     } else {
+//         document.addEventListener("DOMContentLoaded", fn);
+//
+//         console.log('boop');
+//     }
+// }
+// let audio1 = document.getElementById("audioID");
+// // console.log(audio1);
+// function playAudio() {
+//   audio1.play();
+//   console.log('play');
+// }
